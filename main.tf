8c38283b446c25b14e85daf929279be947e67d73
rushikeshvibhute678-byte <rushikeshvibhute678@gmail.com>
2025-11-08 22:19:58 +0530

    Add initial Terraform configuration for AWS EC2

---
+++ main.tf
@@ -0,0 +1,25 @@
+# Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      #version = "~> 3.21" # Optional but recommended in production
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  C:\Users\hp\.aws\credentials
+  region  = "ap-south-1"
+}
+
+# Resource Block
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-0305d3d91b9f22e84" # Amazon Linux in us-east-1, update as per your region
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terra_cloud_vm"
+  }
+}
